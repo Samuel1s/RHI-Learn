6ae4fda35a956c40419305a2369e42659d6f0aa9
Samuel1s <32439435+Samuel1s@users.noreply.github.com>
2021-12-10 16:06:10 -0300

    Files Updated.

---
+++ src/js/azure/section-5.js
@@ -0,0 +1,28 @@
+export const privacyPolicies = {
+    template: `
+        <div class="slide paper"> 
+            <h2 class="mb-3">Azure - Privacy Policies.</h2>
+            <ul>
+               <li>The <a href="https://privacy.microsoft.com/pt-BR/privacystatement" target="_blank">Microsoft Privacy Statement</a> provides trust in how Microsoft collects, protects, and uses customer data.</li>
+               <li>The <a href="https://www.microsoft.com/trustcenter" target="_blank">Trust Center</a> provides you with documentation about compliance standards and how Azure can support your business.</li>
+               <li>The <a href="https://docs.microsoft.com/en-us/azure/compliance/" target="_blank">Azure compliance documentation</a> includes detailed information about legal and regulatory standards and compliance on Azure.</li>
+            </ul>
+            <h4 class="mb-3">A little bit about security</h4>
+            <div class="list-def">
+                <p>
+                    <strong>Azure Key Vault</strong> stores application secrets (such as passwords, encryption keys, and certificates) in one centralized location.
+                    Azure Dedicated Host provides dedicated physical servers to host Azure VMs for Windows and Linux.
+                </p>
+                <p>
+                    <strong>Azure Dedicated Host</strong> provides dedicated physical servers to host Azure VMs for Windows and Linux.
+                </p>
+                <p>
+                    <strong>Azure Sentinel</strong> aggregates security data from a variety of different sources and provides additional functionality for detecting – and responding to – threats.
+                </p>
+                <p>
+                    <strong>Azure Security Center</strong> provides visibility into the security posture across all services, both in Azure and locally.
+                </p>
+            </div>
+        </div>
+    `
+}
